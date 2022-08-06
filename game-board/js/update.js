70d96b4052cd02fe29fdc894adc2a6d01e7a6c2f
Mark Ustby <ustbymark@gmail.com>
2022-08-06 17:22:36 -0500

    implemented rooms for when a game is created or the lobby of the game is joined for users to connect to the socket room

---
+++ game-board/js/update.js
@@ -0,0 +1,18 @@
+//this file will update the game board
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+canvas.width = window.innerWidth / 3;
+canvas.height = window.innerHeight / 5;
+
+
+
+
+
+
+
+
+function render () {
+   pegHoles.forEach(pegHole => {
+        pegHole.draw();
+    });
+}
