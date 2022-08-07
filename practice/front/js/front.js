6704526e031d64665788196d41df39ca978361f7
LSton40 <logan.sutton@gmail.com>
2022-08-07 15:39:13 -0500

    HBS files from html, more design work

--- practice/front/js/front.js
+++ practice/front/js/front.js
@@ -5,6 +5,7 @@ const teaser = setTimeout(teaserOpening, 4000)
 
 function teaserOpening() {
     document.getElementById("tease").style.display = 'none';
+    // document.getElementsByTagName("body")
     titleMagic();
 }
 
@@ -12,7 +13,8 @@ function titleMagic() {
     // document.getElementById("splash-header").style.display = 'flex';
     document.getElementById("splash_header").style.display = 'flex';
     setTimeout(() => {
-        document.getElementById("main").style.display = 'grid';
+        document.getElementById("main").style.display = 'flex';
+        document.getElementById("splash_header").style.backgroundColor = 'linear-gradient(to bottom right, rgba(0,0,0,1.00), rgb(60, 9, 10));';
 
     }, 2500)
 
