80728080af3c2b80466e130f87d73fb6e4566b41
LSton40 <logan.sutton@gmail.com>
2022-08-07 10:17:28 -0500

    Save, pull, and merge

--- game-board/js/update.js
+++ game-board/js/update.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
-canvas.width = window.innerWidth / 3;
-canvas.height = window.innerHeight / 5;
-
-function render() {
-    pegHoles.forEach(pegHole => {
-        pegHole.draw();
-    });
-}
-=======
 
 
 //get the game board class from GenerateGameBoard.js
@@ -23,4 +11,3 @@ GenerateGameBoard.render();
 
 
 
->>>>>>> 713144e4e2eb70741f3fca3a51c5b9d7866de88e
