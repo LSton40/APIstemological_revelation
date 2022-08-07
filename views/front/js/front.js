eaf67d913f1753156ea9ec11ee67d6e51c595c3c
LSton40 <logan.sutton@gmail.com>
2022-08-07 12:32:51 -0500

    Checking other branches

--- views/front/js/front.js
+++ views/front/js/front.js
@@ -1,1 +1,19 @@
-//JS do it
+
+
+
+const teaser = setTimeout(teaserOpening, 4000)
+
+function teaserOpening() {
+    document.getElementById("tease").style.display = 'none';
+    titleMagic();
+}
+
+function titleMagic() {
+    // document.getElementById("splash-header").style.display = 'flex';
+    document.getElementById("splash_header").style.display = 'flex';
+    setTimeout(() => {
+        document.getElementById("main").style.display = 'grid';
+
+    }, 2500)
+
+}
