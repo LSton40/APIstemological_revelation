1462ebcbfe152da91a0ae47e7535bf674c2b745d
Mark Ustby <ustbymark@gmail.com>
2022-08-07 19:08:02 -0500

    working on router

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -1,5 +1,12 @@
+require('dotenv').config();
 const view_router = require('express').Router();
 const { loggedIn } = require('../controllers/helper_functions');
+const { GameBoard } = require('../models/GameBoard.model');
+
+//create socket connection to server
+const io = require('socket.io')();
+
+
 
 // view_router.get('/lobby', loggedIn, (req, res) => {
 // //check if the socket.id of the user matches that of the same user in a database
@@ -17,7 +24,7 @@ view_router.get('/', loggedIn, (req, res) => {
 
     //if they are signed in, then send them to the lobby
     //if they are not signed in, then send them to the login page
-    res.render('index', { username: req.session.username});
+    res.render('index', { username: req.session.username });
 
 });
 
@@ -44,6 +51,47 @@ view_router.get('/register', loggedIn, (req, res) => {
 view_router.get('/gameboard', loggedIn, (req, res) => {
     res.render('gameboard');
 });
+
+
+view_router.get('/gameboard/:id', loggedIn, async (req, res) => {
+    const { id } = req.params;
+    console.log(req.params);
+    console.log('this path works');
+    const game = await GameBoard.findOne({
+        where: {
+            id: id
+        }
+    }).then(game => {
+        if (game) {
+            res.render('gameboard', { game: game });
+        } else {
+            res.render('gameboard');
+        }
+
+
+
+    }).catch(err => {
+        console.log(err);
+    });
+
+    if (game) {
+        //adds the user to the namespace and then redirects to the gameboard
+    
+        res.render('gameboard');
+
+       
+
+
+
+
+
+    } else {
+        res.redirect('/');
+    }
+});
+
+
+
 
 
 // function test(){
