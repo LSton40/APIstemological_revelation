57509fc49aac9ec8751c6dabd1b840246032d56a
LSton40 <logan.sutton@gmail.com>
2022-08-07 19:22:45 -0500

    Updated CSS, FrontJS,  indexHBS

--- views/front/js/front.js
+++ views/front/js/front.js
@@ -2,16 +2,15 @@ const teaser = setTimeout(teaserOpening, 4000)
 
 function teaserOpening() {
     document.getElementById("tease").style.display = 'none';
-    // document.getElementsByTagName("body")
     titleMagic();
 }
 
 function titleMagic() {
-    // document.getElementById("splash-header").style.display = 'flex';
     document.getElementById("splash_header").style.display = 'flex';
     setTimeout(() => {
         document.getElementById("main").style.display = 'flex';
-        document.getElementById("splash_header").style.backgroundColor = 'linear-gradient(to bottom right, rgba(0,0,0,1.00), rgb(60, 9, 10));';
-    }, 2500)
+        document.getElementById("opening").style.background = 'transparent';
+
+    }, 4500)
 
 }
