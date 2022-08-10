8a20f6b2a8c9f3f41ec881be57d6b591f7a3c334
fixedOtter <fo1152rc@go.minnstate.edu>
2022-08-10 07:27:14 -0500

    tried things

--- models/GameBoard.model.js
+++ models/GameBoard.model.js
@@ -17,14 +17,14 @@ GameBoard.init({
   //if the game is ongoin, or if it is finished
   gameStatus: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: true,
     defaultValue: 'active'
 
   },
   //json object containing a list of the users in the game
   gamePlayers: {
     type: DataTypes.JSON,
-    allowNull: false,
+    allowNull: true,
     // get() { // when getting, the object will be parsed from json
     //   return JSON.parse(this.getDataValue("gamePlayers"));
     // },
@@ -35,20 +35,12 @@ GameBoard.init({
   //keeps track of the current turn of the game(could either be a int or maybe the name of the player)
   gameTurn: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: true
   },
-  //json object of the game board
-  // gameBoard: {
-  //   type: DataTypes.JSON,
-  //   allowNull: true,
-  //   get() { // when getting, the object will be parsed from json
-  //     return JSON.parse(this.getDataValue("gameBoard"));
-  //   },
-  //   set() { // when setting, the object will be parsed to json
-  //     return this.setDataValue("gameBoard", JSON.stringify(value));
-  //   }
-  // },
-  //game winner (could be a int or maybe the name of the player). this will be null if the game is not finished
+  tempGamePlayers: {
+    type: DataTypes.JSON,
+    allowNull: true
+  },
   gameWinner: {
     type: DataTypes.INTEGER,
     allowNull: true,
